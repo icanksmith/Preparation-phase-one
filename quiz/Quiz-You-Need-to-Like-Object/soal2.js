42604e004067eca2e4e37120ee670ad380ea0d01
icank smith <sammykitkat8@gmail.com>
2025-11-27 20:12:07 +0700

    clear quiz object soal 2

---
+++ quiz/Quiz-You-Need-to-Like-Object/soal2.js
@@ -0,0 +1,102 @@
+/*
+=========
+HACKATHON
+=========
+
+[INSTRUCTION]
+Buatlah suatu aplikasi untuk membuat catatan ekonomi.
+
+[EXAMPLE]
+"bank account sudah disediakan"
+
+input: [['Jeff Bezos+5%', 'Larry Page+10%', 'Jeff Bezos-3%'], ['Larry Page+2%', 'Larry Page-1%']]
+process:
+  bank account => deposit atas nama Jeff Bezos ditambah 5%, menjadi 105000
+  bank account => deposit atas nama Larry Page ditambah 10%, mejadi  104500
+  bank account => deposit atas nama Jeff Bezos dikurangi 3%, mejadi 101850
+  ...dst
+output:
+  [
+    { name: 'Jeff Bezos', deposit: 105000, owner: 'Amazon' },
+    { name: 'Larry Page', deposit: 104500, owner: 'Google' },
+    { name: 'Jeff Bezos', deposit: 101850, owner: 'Amazon' },
+    { name: 'Larry Page', deposit: 106590, owner: 'Google' },
+    { name: 'Larry Page', deposit: 105524.1, owner: 'Google' }
+  ]
+
+[RULES]
+- Dilarang menggunakan .indexOf(), .split(), .filter(), .map(), dan .slice()
+*/
+
+function economyChangeSummary(tradeActivity) {
+  let duitJeff = 100000
+  let duitLarry = 95000
+  let duitJack = 90000
+
+  let result = [];
+
+  for (let i = 0; i < tradeActivity.length; i++){
+    for (let j = 0; j < tradeActivity[i].length; j++){
+      const data = tradeActivity[i][j];
+      const name = data.replace(/[^a-zA-Z\s]/g, '').trim();
+      const operator = data.replace(/[^+-]/g, '').trim();
+      const digit = Number(data.replace(/[^0-9]/g, '').trim()) / 100;
+      let owner = '';
+      
+      if (name === 'Jeff Bezos') owner = 'Amazon';
+      else if (name === 'Larry Page') owner = 'Google';
+      else if (name === 'Jack Ma') owner = 'Alibaba';
+
+      let newDeposit = 0;
+      if (name === 'Jeff Bezos') newDeposit = duitJeff;
+      else if (name === 'Larry Page') newDeposit = duitLarry;
+      else if (name === 'Jack Ma') newDeposit = duitJack;
+      
+      if (operator === '+'){
+        newDeposit = newDeposit + (newDeposit * digit);
+      } else {
+        newDeposit = newDeposit - (newDeposit * digit);
+      }
+
+      let list = {
+        name: name,
+        deposit: newDeposit,
+        owner: owner
+      }
+      result.push(list);
+
+      if (name === 'Jeff Bezos') duitJeff = newDeposit;
+      else if (name === 'Larry Page') duitLarry = newDeposit;
+      else if (name === 'Jack Ma') duitJack = newDeposit;
+    }
+  }
+  return result;
+}
+
+
+console.log(economyChangeSummary([
+  ['Jeff Bezos+5%', 'Larry Page+10%', 'Jeff Bezos-3%'],
+  ['Larry Page+2%', 'Larry Page-1%'],
+  ['Jack Ma+4%'],
+  ['Larry Page-8%', 'Jack Ma+20%', 'Jeff Bezos-3%', 'Jeff Bezos+8%']
+]));
+/* 
+  [ { name: 'Jeff Bezos', deposit: 105000, owner: 'Amazon' },
+  { name: 'Larry Page', deposit: 104500, owner: 'Google' },
+  { name: 'Jeff Bezos', deposit: 101850, owner: 'Amazon' },
+  { name: 'Larry Page', deposit: 106590, owner: 'Google' },
+  { name: 'Larry Page', deposit: 105524.1, owner: 'Google' },
+  { name: 'Jack Ma', deposit: 93600, owner: 'Alibaba' },
+  { name: 'Larry Page', deposit: 97082.172, owner: 'Google' },
+  { name: 'Jack Ma', deposit: 112320, owner: 'Alibaba' },
+  { name: 'Jeff Bezos', deposit: 98794.5, owner: 'Amazon' },
+  { name: 'Jeff Bezos', deposit: 106698.06, owner: 'Amazon' } ]
+*/
+console.log("==============================================================================");
+
+console.log(economyChangeSummary([
+  ['Jeff Bezos-10%']
+]))
+/*
+  [ { name: 'Jeff Bezos', deposit: 90000, owner: 'Amazon' } ]
+*/
